2bf9b57979f099f079c4d3911718771e399b27cb
Matthieu Fraiz <matthieu.fraiz@epitech.eu>
2023-02-07 11:51:19 +0100

    fix: bad case

--- src/Handler.hpp
+++ src/Handler.hpp
@@ -7,29 +7,30 @@
 
 #pragma once
 //This file and class will need a rename
-#include "IComponent.hpp"
-#include "PinLink.hpp"
 #include <vector>
 #include <map>
 #include <string>
+#include <memory>
+#include "IComponent.hpp"
+#include "PinLink.hpp"
 
 class Handler
 {
     public:
-        void load_file(const std::string &file_name);
-        void read_input();
+        void loadFile(const std::string &file_name);
+        void readInput();
 
     protected:
     private:
-        std::map<std::string, nts::IComponent> _components;
+        std::map<std::string, std::unique_ptr<nts::IComponent>> _components;
         std::vector<PinLink> _links;
         std::size_t _tick;
 
-        void add_chipset(const std::string &type, const std::string &name);
-        void add_link(const std::string &name_1, std::size_t pin_1,
-                      const std::string &name_2, std::size_t pin_2);
+        void addChipset(const std::string &type, const std::string &name);
+        void addLink(const std::string &name1, std::size_t pin1,
+                      const std::string &name2, std::size_t pin2);
 
-        void set_input(const std::string &name, std::size_t value);
+        void setInput(const std::string &name, std::size_t value);
         void display() const;
         void simulate();
         void loop();

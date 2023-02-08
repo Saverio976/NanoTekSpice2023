924c50ad5f2d14d4e2b1ea1207b60b17495476bf
Xavier Mitault <xavier.mitault@epitech.eu>
2023-02-08 18:54:08 +0100

    fix: FileParser read parse

--- src/FileParser.cpp
+++ src/FileParser.cpp
@@ -8,6 +8,7 @@
 #include <cstddef>
 #include <fstream>
 #include <ios>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include "Handler.hpp"
@@ -56,32 +57,37 @@ std::size_t FileParser::parseLinkPin(const std::string &all) const
 void FileParser::loadFile(const std::string &fileName)
 {
     std::string line;
-    std::stringstream linestream;
     std::ifstream f;
     ParsingType type = UNKNOWN;
-    std::string rightPart;
-    std::string leftPart;
+
 
     f.open(fileName);
     if (f.bad() || !f.good() || !f.is_open()) {
         throw FileParsingError("Unable to open file " + fileName);
     }
     while (std::getline(f, line)) {
-        if (line.starts_with("#")) {
+        std::string rightPart;
+        std::string leftPart;
+        std::stringstream linestream(line);
+        linestream.exceptions(std::ios::failbit);
+        if (line.starts_with("#") || line == "") {
+            std::cout << "Ignore: # or empty line" << std::endl;
             continue;
         }
         if (line.starts_with(".chipsets:")) {
+            std::cout << "Chipsets: " << std::endl;
             type = CHIPSETS;
             continue;
         } else if (line.starts_with(".links:")) {
+            std::cout << "Links: " << std::endl;
             type = LINKS;
             continue;
         }
-        linestream.str(line);
-        linestream >> leftPart >> rightPart;
         if (type == CHIPSETS) {
+            std::cout << "addChipset" << std::endl;
             this->_handler->addChipset(leftPart, rightPart);
         } else if (type == LINKS) {
+            std::cout << "addLink" << std::endl;
             this->_handler->addLink(
                 this->parseLinkName(leftPart), this->parseLinkPin(leftPart), 
                 this->parseLinkName(rightPart), this->parseLinkPin(rightPart)

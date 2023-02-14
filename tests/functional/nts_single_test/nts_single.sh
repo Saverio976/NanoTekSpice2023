d7d136dffc6c442fdd3e436afb3fd9c44940fac3
Matthieu Fraiz <matthieu.fraiz@epitech.eu>
2023-02-14 15:16:21 +0100

    fix: moved tests hierarchy
    It's better now :)

---
+++ tests/functional/nts_single_test/nts_single.sh
@@ -0,0 +1,15 @@
+error_happend=0
+
+for FILE in $(ls tests/nts_files/single/*.nts | sort)
+do
+    error_msg=$(echo -n "" | ./nanotekspice $FILE 2>&1 >/dev/null)
+    if [ $? -ne 0 ]
+    then
+        error_happend=84
+        echo "Failed to load $FILE"
+        echo "error: [$error_msg]"
+        echo ""
+    fi
+done
+
+exit $error_happend
